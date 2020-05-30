f3b51264e9afe76d7f17a734085d241c18e76efa
nehal ali <nehalali240@yahoo.com>
2020-05-30 23:44:43 +0200

    adding the Expense new UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AddComponent } from '../components/add/add.component';
 import {ModalController} from '@ionic/angular';
+import { Expense } from '../expense';
+import { ExpenseService } from '../expense.service';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +10,22 @@ import {ModalController} from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+   selectedDate : Date;
+   TotalSpending : number;
+   expenses : Expense[];
+   
 
-  constructor(public  modalController: ModalController) {
+  constructor(public  modalController: ModalController , public Servicecontrollor : ExpenseService) {
+     this.TotalSpending = 0;
+     this.expenses = this.Servicecontrollor.Expensesarray;
   }
 
   async presentModal() {
     const modal = await this.modalController.create({
       component: AddComponent , 
-      componentProps : {value : 123}
+      componentProps : {date : this.selectedDate}
     });
+   
     return await modal.present();
   }
 
