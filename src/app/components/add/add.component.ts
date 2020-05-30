20d9d1d4e2f0aec944e2f5ed2e253651e9dddbba
nehal ali <nehalali240@yahoo.com>
2020-05-30 23:44:01 +0200

    Modifying some functionality

--- src/app/components/add/add.component.ts
+++ src/app/components/add/add.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {ModalController} from '@ionic/angular';
 import { Expense } from 'src/app/expense';
 import { ToastController } from '@ionic/angular';
+import { ExpenseService } from 'src/app/expense.service';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.scss'],
 })
+
+
 export class AddComponent implements OnInit {
+  @Input('date') date:Date;
   ExpenseTypes : any[] = ['Gerneral' , 'Food' , 'Groceries' , 'Movies' , 'clothing' , 'Meds' , 'Games' , 'Sports' , 
                           'Electronics' , 'Furniture' , 'Rent' , 'gifts' , 'Insurance' , 'Taxes' , 'Gas' , 'internet' , 'Mobile'
                           , 'Other'];
 
-  constructor(public  modalController: ModalController , public toastcontroller: ToastController) {
+  constructor(public  modalController: ModalController , public toastcontroller: ToastController , public Expenseserv : ExpenseService) {
   }
 
   async presentToast() {
@@ -37,6 +41,10 @@ export class AddComponent implements OnInit {
       this.presentToast();
       return;
     }
+    this.date = new Date(this.date);
+    const ex = new Expense(Number(amount.value) ,type.value ,  desc.value , this.date.toDateString());
+    this.Expenseserv.addExpense_toarray(ex);
+    this.dismiss();
   }
 
   ngOnInit() {}
