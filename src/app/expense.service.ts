c3259ca682eaece13b5b5d3fcfd6b63f9bf1f87a
nehal ali <nehalali240@yahoo.com>
2020-06-04 21:22:27 +0200

    adding change Date event

--- src/app/expense.service.ts
+++ src/app/expense.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Expense } from './expense';
+import { BehaviorSubject } from 'rxjs';
+import{Storage} from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +11,89 @@ export class ExpenseService {
   todayTotal : number;
   selectedDate : Date;
   Expensesarray : Expense[];
+  todayamount : BehaviorSubject<number>;
+  Expenseobserv :  BehaviorSubject<Expense[]>;
   
-
   constructor() { 
     this.Expensesarray = [];
+    this.todayTotal = 0;
+    this.todayamount = new BehaviorSubject(this.todayTotal);
+    this.todayamount.asObservable();
+    this.Expenseobserv = new BehaviorSubject(this.Expensesarray);
+    this.Expenseobserv.asObservable();
+    
   }
 
   addExpense_toarray (x : Expense)
   {
     this.Expensesarray.unshift(x);
     this.addtotal(x.amount);
+    this.SaveExpensetostorage(x.date).then(success =>{
+      this.Expenseobserv.next(this.Expensesarray);
+    })
   }
 
   addtotal (value : number)
   {
     this.todayTotal += value;
+   // console.log(this.todayTotal);
+   this.todayamount.next(this.todayTotal);
   }
+
+  async SaveExpensetostorage (date : string)
+  {
+    await Storage.set({key : date , value : JSON.stringify(this.Expensesarray)});
+  }
+
+  //async clear() {
+   // await Storage.clear();
+  //} 
+  async getExpenseFromStorage ()
+  {
+  // this.clear();
+    await Storage.get({key : this.getcurrentDate()}).then(value => {
+      this.Expensesarray = [];
+      const objects: Expense[] = JSON.parse(value.value);
+      if (objects != null)
+      this.Expensesarray = objects;
+      else
+      this.Expensesarray = [];
+    })
+  }
+
+  getcurrentDate()
+  {
+    return new Date().toDateString();
+  }
+
+
+
+  intitamount()
+  {
+    let totalamount2 : number = 0;
+    this.Expensesarray.forEach(element => {
+      totalamount2 += element.amount;
+    });
+    this.todayTotal = Math.round(totalamount2);
+    this.todayamount.next(this.todayTotal);
+  }
+
+
+  async getspecific_Data(date : Date)
+  {
+    this.selectedDate = date;
+   // console.log(date.toDateString);
+    await Storage.get({key:date.toDateString()}).then(value => {
+      this.Expensesarray = [];
+      const objects: Expense[] = JSON.parse(value.value);
+      if (objects != null)
+      this.Expensesarray = objects;
+      else
+      this.Expensesarray = [];
+   
+    this.Expenseobserv.next(this.Expensesarray);
+  });
+  this.intitamount();
+  }
+
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,7 @@ import { AddComponent } from '../components/add/add.component';
 import {ModalController} from '@ionic/angular';
 import { Expense } from '../expense';
 import { ExpenseService } from '../expense.service';
-
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -15,18 +15,51 @@ export class HomePage {
    expenses : Expense[];
    
 
-  constructor(public  modalController: ModalController , public Servicecontrollor : ExpenseService) {
-     this.TotalSpending = 0;
-     this.expenses = this.Servicecontrollor.Expensesarray;
+  constructor(public  modalController: ModalController , public Servicecontrollor : ExpenseService , public toastcontroller: ToastController ) {
+   // this.TotalSpending =0;
+    // this.expenses = this.Servicecontrollor.Expensesarray;
+    this.Servicecontrollor.todayamount.subscribe(value => {
+     this.TotalSpending  = value;
+   });
+   this.Servicecontrollor.Expenseobserv.subscribe(value => {
+    this.expenses  = value;
+  });
+   this.selectedDate=null;
+   if(this.selectedDate == null)  this.TotalSpending = 0;
   }
 
+
+  async presentToast() {
+    const toast = await this.toastcontroller.create({
+      message: 'please Select a valid date first to put your expenses in',
+      duration: 1500 , 
+      color : 'warning' , 
+      position:"top"
+    });
+    toast.present();
+  }
+
+
   async presentModal() {
+    if(this.selectedDate === null)
+    {
+      this.presentToast();
+      return;
+    }
     const modal = await this.modalController.create({
       component: AddComponent , 
       componentProps : {date : this.selectedDate}
     });
    
     return await modal.present();
+  }
+
+  getspecificData()
+  {
+    //console.log(this.selectedDate);
+    this.Servicecontrollor.getspecific_Data(new Date(this.selectedDate|| new Date())).then(res => {
+      this.Servicecontrollor.intitamount();
+    });
   }
 
 }
